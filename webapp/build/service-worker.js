e6827993ba9768c07c94bbe7d78e1e91e2f2f1de
sirily11 <sirily1997@gmail.com>
2020-04-09 22:02:37 -0500

    Added markdown import

--- webapp/build/service-worker.js
+++ webapp/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.94eaf1fe569bbcef26526d17f7c841d1.js"
+  "./precache-manifest.b08bccd7ce897c03f07d4b12c766c522.js"
 );
 
 self.addEventListener('message', (event) => {
